5a062a18c7ac1c99b68d804d9e49cdce4af44d06
Kevin Pheasey <kevin.pheasey@gmail.com>
2025-01-18 10:17:32 -0500

    feat(package): update yarn, add missing comma, update build (dist), add build script, update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+const path = require('path');
+
 module.exports = {
-    context: __dirname + "/dist",
-    // ...
+    context: __dirname,
+    entry: './src/index.js',
     output: {
         libraryTarget: "commonjs",
-        // ...
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
     },
     externals: ["react", "fuzzy-match-utils"],
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
 };
